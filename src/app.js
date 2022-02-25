19e1b3081b96798857112bc1ca1a56b2bf6c171c
tiohs <hamiltonsilva551@gmail.com>
2022-02-25 21:18:32 +0100

    teste update

--- src/app.js
+++ src/app.js
@@ -30,11 +30,23 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = body.params;
+
+  const repository = repositories.find(e => repositories.indexOf() == id);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const findRepositoryIndex = repositories.findIndex(repository => 
+    repository.id == id
+  );
+  if (findRepositoryIndex >= 0) {
+    repositories.splice(findRepositoryIndex, 1);
+  } else {
+    return response.status(400).json({ error : 'Erro ao encontrar repositorio '})
+  }
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
